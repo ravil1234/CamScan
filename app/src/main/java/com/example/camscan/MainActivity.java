e9f6fe8e93286141cf6c43367b970ddada965a81
Ravil <mohdravil000@gmail.com>
2020-07-07 14:51:32 +0530

    Cam Scan initial commit

---
+++ app/src/main/java/com/example/camscan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.camscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
