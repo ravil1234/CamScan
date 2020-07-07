74b0431bb2787dde7bef76724239aded1c4bb955
Ravil <mohdravil000@gmail.com>
2020-07-07 14:58:16 +0530

    new commit

--- app/src/main/java/com/example/camscan/Activities/MainActivity.java
+++ app/src/main/java/com/example/camscan/Activities/MainActivity.java
@@ -1,8 +1,7 @@
-package com.example.camscan;
-
+package com.example.camscan.Activities;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.example.camscan.R;
 
 public class MainActivity extends AppCompatActivity {
 
