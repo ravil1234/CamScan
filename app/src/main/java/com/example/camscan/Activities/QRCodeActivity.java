4c684243b97f5f1b4bf8292eb184e33a39724aba
Ravil <mohdravil000@gmail.com>
2020-08-08 11:00:57 +0530

    scanqr

---
+++ app/src/main/java/com/example/camscan/Activities/QRCodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.camscan.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+    }
+}
