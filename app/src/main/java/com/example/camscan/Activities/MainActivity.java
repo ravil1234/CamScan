1e0d75c1ff7ec665dff5f634791fd16e4eb2fd92
Ravil <mohdravil000@gmail.com>
2020-09-03 20:35:07 +0530

    design homescreen done

--- app/src/main/java/com/example/camscan/Activities/MainActivity.java
+++ app/src/main/java/com/example/camscan/Activities/MainActivity.java
@@ -2,12 +2,11 @@ package com.example.camscan.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.camscan.R;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homescreen_design);
+        getSupportActionBar().hide();
     }
 }
